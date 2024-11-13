c8046645c446352acfbc402d5f7628f63a459d92
cobrazohaib56 <zohaibaamer2001@gmail.com>
2024-11-13 19:32:35 +0500

    Added new serach index and also changed the placing of classification prompt

--- Azure_RAG_Final.py
+++ Azure_RAG_Final.py
@@ -7,10 +7,10 @@ import streamlit as st
 load_dotenv()
 
 # Define your API base URL, deployment ID, and API key
-#api_key = os.getenv("OPENAI_API_KEY")  # Replace with your actual API key
-api_key = st.secrets["OPENAI_API_KEY"]
-#azure_search_key = os.getenv("AZURE_AI_SEARCH_KEY")
-azure_search_key = st.secrets["AZURE_AI_SEARCH_KEY"]
+api_key = os.getenv("OPENAI_API_KEY")  # Replace with your actual API key
+# api_key = st.secrets["OPENAI_API_KEY"]
+azure_search_key = os.getenv("AZURE_AI_SEARCH_KEY")
+# azure_search_key = st.secrets["AZURE_AI_SEARCH_KEY"]
 # Define the endpoint URL
 url = "https://relationwiseai4136950365.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2024-08-01-preview"
 
@@ -23,10 +23,12 @@ headers = {
 def azure_payload(user_input, convo_history):
     # Define the payload
     payload = {
+
     "messages": [
         {
         "role": "system",
         "content": f"""You are an AI assistant that provides answers from the PDFs and your General Knowledge Base, when nothing from the publications is found.
+        
         You have to keep in mind the history or the context of the converstaion which is {convo_history}"""
         },
         {
@@ -46,7 +48,7 @@ def azure_payload(user_input, convo_history):
         "parameters": {
             "filter": None,
             "endpoint": "https://relationwise-ai-searchservice2.search.windows.net",
-            "index_name": "vector-search-index-1",
+            "index_name": "vector-search-index-2",
             "semantic_configuration": "azureml-default",
             "authentication": {
             "type": "api_key",
@@ -82,17 +84,18 @@ def azure_payload(user_input, convo_history):
         if res["choices"][0]["message"]["context"]["citations"]:
 
             payload = {
+
                 "messages": [{
                     "role": "system",
                     "content": f"""
                     You need to check the relevence of both "Number 1" and "Number 2. You have to handle 2 things here.
                     
-                    NOTE: Do not say anything about their relevence in your response
-
-                    If both "Number 1" and "Number 2" are not related, you need to give a general response for the user query which is the 
-                    Number 1, this one : {user_input}. Please Keep in mind you need to give a response of it from your own Knowledge base.
+                    NOTE: Do not say anything about their relevence in your response. Make sure not to repeat the {user_input} in the response you give.
                     
-                    If both of "Number 1" and "Number 2" are related you strictly need to say "Yes"  """
+                    If both of "Number 1" and "Number 2" are related you strictly need to say "Yes"  
+                    
+                    If both "Number 1" and "Number 2" are not related, you need to give a general response for the user query which is the 
+                    Number 1, this one : {user_input}. Please Keep in mind you need to give a response of it from your own Knowledge base."""
                     },
                     {
                     "role": "user",
@@ -102,7 +105,7 @@ def azure_payload(user_input, convo_history):
                     }],
                 "temperature": 0.7,
                 "top_p": 0.95,
-                "max_tokens": 800
+                "max_tokens": 800,
             }
             response_2 = requests.post(url, headers=headers, data=json.dumps(payload))
             if response_2.status_code == 200:
@@ -121,6 +124,7 @@ def azure_payload(user_input, convo_history):
 
         else:
             payload = {
+
                 "messages": [{
                     "role": "system",
                     "content": f"""You are an AI assistant that provides answers from the PDFs and your General Knowledge Base when noting from the publications is found.
@@ -132,7 +136,7 @@ def azure_payload(user_input, convo_history):
                     }],
                 "temperature": 0.7,
                 "top_p": 0.95,
-                "max_tokens": 800
+                "max_tokens": 800,
             }
             response_1 = requests.post(url, headers=headers, data=json.dumps(payload))
             if response_1.status_code == 200:
